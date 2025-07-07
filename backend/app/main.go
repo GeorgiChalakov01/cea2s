cf50a0f4797ff538830cda8a69e0d7a94f2b9337
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-07-07 18:40:32 +0300

    Working home page. Part1 in progress. Started implementing the docker compose db and minio containers.

---
+++ backend/app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	
+	"github.com/a-h/templ"
+	"github.com/GeorgiChalakov01/cea2s/pages/home"
+	"github.com/GeorgiChalakov01/cea2s/pages/part1"
+)
+
+func main() {
+	// Serve static files (audio) with correct path
+	fs := http.FileServer(http.Dir("./pages/part1/audio"))
+	http.Handle("/audio/", http.StripPrefix("/audio/", fs))
+	
+	// Routes
+	http.Handle("/", http.RedirectHandler("/home", http.StatusSeeOther))
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		templ.Handler(home.Home()).ServeHTTP(w, r)
+	})
+	http.HandleFunc("/part1", func(w http.ResponseWriter, r *http.Request) {
+		templ.Handler(part1.Part1()).ServeHTTP(w, r)
+	})
+	
+	fmt.Println("Listening on :8080")
+	http.ListenAndServe(":8080", nil)
+}
